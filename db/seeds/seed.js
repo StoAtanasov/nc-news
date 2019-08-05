a1fd06a740f3ca22a0403d87c68b35227bff5af3
StoAtanasov <stoatanasov@yahoo.com>
2019-08-05 15:02:55 +0100

    Utils functions created

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -21,14 +21,7 @@ exports.seed = function(connection, Promise) {
       return Promise.all([topicsInsertions, usersInsertions])
         .then(() => {
           
-          /* 
-          
-          Your article data is currently in the incorrect format and will violate your SQL schema. 
-          
-          You will need to write and test the provided formatDate utility function to be able insert your article data.
-      
-          Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-          */
+         
         })
 
 
