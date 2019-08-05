cd9aea51fad69350a64bf11be85cbdb170e6adb6
StoAtanasov <stoatanasov@yahoo.com>
2019-08-05 15:56:47 +0100

    File app.js created

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,7 +1,29 @@
 process.env.NODE_ENV = "test";
 const { expect } = require("chai");
 const chaiSorted = require("chai-sorted");
+const chai = require("chai");
 chai.use(chaiSorted);
 const request = require("supertest");
 const connection = require("../db/connection");
 const app = require("../app");
+
+describe("app", () => {
+  after(() => {
+    return connection.destroy();
+  });
+  beforeEach(() => {
+    return connection.seed.run();
+  });
+  describe("/*", () => {
+    describe("/api", () => {
+      it("GET / status:200, returns a body with all endpoints.", () => {
+        return request(app)
+        .get("/api")
+        .status(200)
+        .then(({body}) => {
+          expect(body).to.be.an("object");
+        })
+      });
+    });
+  });
+});
