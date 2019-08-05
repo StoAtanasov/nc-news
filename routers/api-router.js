6d3473b29a5d004f0b055a8445f849dac230368b
StoAtanasov <stoatanasov@yahoo.com>
2019-08-05 21:04:01 +0100

    /api/users/ route created

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,7 +1,10 @@
 const apiRouter = require("express").Router();
 const endPoints = require("../endpoints.json");
-const topicsRouter = require("../routers/topics-router")
+const topicsRouter = require("../routers/topics-router");
+const usersRouter = require("./users-router");
 
 apiRouter.route("/").get((req,res) => res.status(200).send(endPoints));
 apiRouter.use("/topics",topicsRouter);
+apiRouter.use("/users", usersRouter);
+
 module.exports = apiRouter;
