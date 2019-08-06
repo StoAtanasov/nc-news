798d93e152e231091b3c25cd4e373b0cac0bce32
StoAtanasov <stoatanasov@yahoo.com>
2019-08-06 20:46:35 +0100

    Function selectAllComments required from the comments model

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,13 +1,25 @@
-const {createCommentByArticle} = require("../models/comments-model")
+const {
+  createCommentByArticle,
+  selectAllComents
+} = require("../models/comments-model");
+
 exports.sendNewArticleComment = (req, res, next) => {
   const { article_id } = req.params;
-  const {username, body} = req.body;
-createCommentByArticle(article_id, username,body)
-.then(comment => {
-  res.status(201).send({comment})
-})
-.catch(err => {
-  next(err)
-});
- 
+  const { username, body } = req.body;
+  createCommentByArticle(article_id, username, body)
+    .then(comment => {
+      res.status(201).send({ comment });
+    })
+    .catch(err => {
+      next(err);
+    });
 };
+
+exports.sendAllComments = (req,res,next) => {
+  const {article_id} = req.params;
+  console.log(article_id);
+  selectAllComents(article_id)
+  .then(comments => {
+    res.status(200).send({comments})
+  })
+}
