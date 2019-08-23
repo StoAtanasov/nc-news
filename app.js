aae494b39bc51d248a8c4ba24b827f025e705778
StoAtanasov <stoatanasov@yahoo.com>
2019-08-23 14:04:37 +0100

    Enable cross-site HTTP requests

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
 const {handlePSQLErrors, handleCustomErrors, handle500} = require("./errors")
+const cors = require("cors");
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
